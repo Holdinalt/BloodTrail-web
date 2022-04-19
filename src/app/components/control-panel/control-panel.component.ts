b2f2fb351f0acf62909079bfc0939a7227966bd7
Holdest <zbsholdest@mail.ru>
2022-04-20 02:21:01 +0300

    I hate animation

---
+++ src/app/components/control-panel/control-panel.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {NavPanelHandlerService} from "../../nav-panel-handler.service";
+import {NavbarPieceModel} from "../../models/navbar-piece.model";
+
+@Component({
+  selector: 'app-control-panel',
+  templateUrl: './control-panel.component.html',
+  styleUrls: ['./control-panel.component.css']
+})
+export class ControlPanelComponent implements OnInit {
+
+  nav?: NavbarPieceModel[]
+
+  constructor(public navPanelHandlerService: NavPanelHandlerService) {
+    this.nav = navPanelHandlerService.getNavbarPieces();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/components/games/league-of-legends/league-of-legends.component.ts
+++ src/app/components/games/league-of-legends/league-of-legends.component.ts
@@ -18,4 +18,32 @@ export class LeagueOfLegendsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  toggleSpoiler(checkBox: HTMLElement, elem: HTMLElement, event: Event){
+    // @ts-ignore
+    if(!checkBox.checked){
+      elem.classList.remove("closed");
+      elem.classList.add("opened");
+      elem.style.marginBottom = "inherit";
+
+    } else{
+      elem.classList.remove("opened")
+      elem.classList.add("closed")
+      elem.style.marginBottom = String(-elem.offsetHeight) + "px";
+
+    }
+    // @ts-ignore
+    checkBox.checked = !checkBox.checked;
+    this.toggleSpoilerImg(event.target as HTMLElement);
+
+  }
+
+  toggleSpoilerImg(img: HTMLElement){
+    if(img.style.transform){
+      img.style.transform = '';
+    } else{
+      img.style.transform = 'rotate(' + 180 + 'deg)';
+    }
+  }
+
 }
